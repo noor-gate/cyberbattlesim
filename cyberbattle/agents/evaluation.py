21ba1613794289b8d029a5d8199c2a702bad40b9
Noor <noorsawhney@yahoo.co.uk>
2023-05-18 14:00:40 +0100

    fix eval

--- cyberbattle/agents/evaluation.py
+++ cyberbattle/agents/evaluation.py
@@ -23,10 +23,11 @@ results.close()
 
 
 def eval_agent(training_envs, test_env, ep, network, defender=None):
-    agents = [  # (learner.RandomPolicy(), "Random"),
-        # (tql.QTabularLearner(ep=ep, gamma=0.015, learning_rate=0.01, exploit_percentile=100), "Tabular Q-Learning"),
-        # (dqla.DeepQLearnerPolicy(ep=ep, gamma=0.015, replay_memory_size=10000, target_update=10, batch_size=512, learning_rate=0.01), "Deep Q-Learning"),
-        (PPOLearnerBetter(ep=ep, gamma=0.0015), "PPO")]
+    agents = [(learner.RandomPolicy(), "Random"),
+              (tql.QTabularLearner(ep=ep, gamma=0.015, learning_rate=0.01, exploit_percentile=100), "Tabular Q-Learning"),
+              (dqla.DeepQLearnerPolicy(ep=ep, gamma=0.015, replay_memory_size=10000, target_update=10, batch_size=512, learning_rate=0.01), "Deep Q-Learning"),
+              (PPOLearnerBetter(ep=ep, gamma=0.015), "PPO"),
+              (PPOLearnerBetter(ep=ep, gamma=0.015), "PPO Curiosity")]
 
     trained_agents = []
 
